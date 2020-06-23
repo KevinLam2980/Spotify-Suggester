0be08dd820b3e68dd6a1241ee4d8039e7d4ab9ce
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-23 14:00:10 -0700

    Sign up posting to backend

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,20 +1,89 @@
-import React from "react"
+import React, { useState } from "react"
 import { useHistory } from "react-router-dom"
+import axios from "axios";
+import axiosWithAuth from "../util/axiosWithAuth";
+
+const emptyUser = {
+    email: "",
+    password: ""
+};
 
 const SignIn = props => {
     const { push } = useHistory();
 
-    const signin = e => {
-        push("/dashboard")
+    const [form, setForm] = useState(emptyUser);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    }
+
+    let createUser = newUser => {
+        axios.post("https://spotify-suggestions-backend.herokuapp.com/auth/signup", newUser)
+            .then(response => {
+                localStorage.setItem('token', response.data.token);
+                push("/login");
+            })
+            .catch(error => {
+                console.log("Error", error)
+            })
+            .finally(() => {
+                setForm(emptyUser);
+            });
+
+    }
+
+    // let createUser = newUser => {
+    //     axiosWithAuth()
+    //         .post("/auth/login", newUser)
+    //         .then(response => {
+    //             localStorage.setItem('token', response.data.token);
+    //             debugger;
+    //             push("/dashboard");
+    //         })
+    //         .catch(error => {
+    //             console.log("Error", error)
+    //         })
+    //         .finally(() => {
+    //             setForm(emptyUser);
+    //         });
+
+    // }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const newUser = {
+            email: form.email.trim(),
+            password: form.password.trim()
+        }
+        createUser(newUser);
+
     }
 
     return (
         <div className="signin-div">
 
             <h2>Connect with Spotify!</h2>
-
-            <button className="signin" onClick={signin}>Sign In</button>
-
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter email"
+                    value={form.email}
+                    onChange={handleChange}
+                />
+                <br />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    value={form.password}
+                    onChange={handleChange}
+                />
+                <br />
+                <button className="signin">Sign up</button>
+            </form>
+            <p>Already registered?</p>
         </div>
     )
 }
