0ea074c13fa6ef28af353e9d56b1032009a4908b
David Temple <dstemple7@gmail.com>
2020-06-24 12:29:39 -0500

    YUP finalized

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { useHistory } from "react-router-dom"
 import axios from "axios";
 import formSchema from '../Validation/formSchema'
@@ -20,9 +20,26 @@ const SignUp = props => {
     const { push } = useHistory();
 
     const [form, setForm] = useState(emptyUser);
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
+    const [disabled, setDisabled] = useState(initialDisabled)
 
     const handleChange = e => {
         const { name, value } = e.target;
+        Yup 
+        .reach(formSchema, name)
+        .validate(value)
+        .then(() => {
+            setFormErrors({
+                ...formErrors,
+                [name]: ""
+            })
+        })
+        .catch(err => {
+            setFormErrors({
+                ...formErrors,
+                [name]: err.errors[0]
+            })
+        })
         setForm({ ...form, [name]: value });
     }
 
@@ -53,6 +70,12 @@ const SignUp = props => {
 
     }
 
+    useEffect(() => {
+        formSchema.isValid(form).then(valid => {
+            setDisabled(!valid)
+        })
+    }, [form])
+
     const handleClick = e => {
         push("/login");
     }
@@ -61,6 +84,10 @@ const SignUp = props => {
         <div className="signin-div">
 
             <h2>Connect with Spotify!</h2>
+            <div className="schemaErrors">
+                <h4>{formErrors.email}</h4>
+                <h4>{formErrors.password}</h4>
+            </div>
             <form onSubmit={handleSubmit}>
                 <input
                     type="email"
@@ -78,7 +105,7 @@ const SignUp = props => {
                     onChange={handleChange}
                 />
                 <br />
-                <button className="signup">Sign up</button>
+                <button className="signup" disabled={disabled}>Sign up</button>
             </form>
             <div>
                 <p>Already registered?
